def90bb2843f60d47404a68a1d792057d71df4fd
Nikhil Kumar <kumar.nikhil9687@gmail.com>
2024-04-12 15:12:01 +0530

    Modified various file to comple handlers and routes

--- BookStore/go.mod
+++ BookStore/go.mod
@@ -2,6 +2,12 @@ module github.com/nikhilkumar9687/GoLangProjects/BookStore
 
 go 1.22.0
 
-require github.com/jinzhu/gorm v1.9.16
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/jinzhu/gorm v1.9.16
+)
 
-require github.com/jinzhu/inflection v1.0.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+)
--- BookStore/pkg/models/book.go
+++ BookStore/pkg/models/book.go
@@ -5,3 +5,46 @@ import (
 	"github.com/nikhilkumar9687/GoLangProjects/BookStore/pkg/config"
 )
 
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:"" json:"name,omitempty"`
+	Author      string `json:"author,omitempty"`
+	Publication string `json:"publication,omitempty"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook () *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById (Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+
+	return &getBook, db
+}
+
+func DeleteBook (Id int64) Book {
+	var deleteBook Book
+	db.Where("ID=?", Id).Delete(deleteBook)
+
+	return deleteBook
+}
