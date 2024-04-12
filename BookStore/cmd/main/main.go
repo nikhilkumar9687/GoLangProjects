217ec492121cfdca5e2fb9b263c4555042838bb5
Nikhil Kumar <kumar.nikhil9687@gmail.com>
2024-04-12 15:53:20 +0530

    Updated main.go

--- BookStore/cmd/main/main.go
+++ BookStore/cmd/main/main.go
@@ -18,6 +18,28 @@ func main() {
 	fmt.Println("Starting Server at 8000")
 	if err := http.ListenAndServe("localhost:9010", r); err != nil {
         fmt.Println("Server failed to start: ", err)
+	}package main
+
+	import (
+		"fmt"
+		//"log"
+		"net/http"
+	
+		"github.com/gorilla/mux"
+		_ "github.com/jinzhu/gorm/dialects/mysql"
+		"github.com/nikhilkumar9687/GoLangProjects/BookStore/pkg/routes"
+	)
+	
+	func main() {
+		fmt.Println("In Main.go")
+		r := mux.NewRouter()
+		routes.RegisterBookStoreRoutes(r)
+		http.Handle("/", r)
+		fmt.Println("Starting Server at 8000")
+		if err := http.ListenAndServe("localhost:9010", r); err != nil {
+			fmt.Println("Server failed to start: ", err)
+		}
+	
 	}
 
 }
